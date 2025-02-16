b175993f2cdaad0e8e809f7e0d3124b792aa0d99
BugHunter-X <askzenai@gmail.com>
2025-02-16 13:28:04 +0100

    Add bugSolution.py file

---
+++ bugSolution.py
@@ -0,0 +1,14 @@
+def function_with_uncommon_error_solution(n, memo={}):
+    if n in memo:
+        return memo[n]
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        result = function_with_uncommon_error_solution(n-1, memo) + function_with_uncommon_error_solution(n-2, memo)
+        memo[n] = result
+        return result
+
+#This will avoid RecursionError by using memoization
+result = function_with_uncommon_error_solution(35)
